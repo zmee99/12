3ef194be15df372a3e08fe2ac1ed19db18dfa718
zmee99 <131580626+zmee99@users.noreply.github.com>
2023-04-25 15:51:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { buyrole1, buyrole2, buyrole3, buyrole4, buylogs, owner, probot, price1, 
 const { Dataa } = require('./DataBase/Models/data');
 require('./DataBase/connect');
 
-const { token } = require('./config.json');
+const { token } = require('./DataBase/config.json');
 
 
 
